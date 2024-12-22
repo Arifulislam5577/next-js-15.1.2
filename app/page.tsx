b24f39463a154838f2ca687662dac146eaeb28ab
Arifulislam5577 <arifalamm33@gmail.com>
2024-12-22 13:06:51 +0600

    First commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="h-screen grid place-items-center">
+      <div className="bg-[var(--background)] w-[120px] border h-[100px] rounded-[--radius]"></div>
+    </div>
+  );
+};
+
+export default Home;
